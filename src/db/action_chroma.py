83a5726690063758ae231298da1a4fe2f5d50f25
Arnur Atentaev <arnurarni@gmail.com>
2025-09-29 05:02:56 +0500

    created Chroma connection

---
+++ src/db/action_chroma.py
@@ -0,0 +1,49 @@
+import logging
+import uuid
+from datetime import datetime
+
+from src.db.chroma_connect import ChromaConnect
+
+from dotenv import load_dotenv
+
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders.pdf import UnstructuredPDFLoader
+from langchain_chroma import Chroma
+
+
+logging.basicConfig(
+    level=logging.ERROR,
+)
+
+
+EMBEDDING_MODEL = "Qwen/Qwen3-Embedding-0.6B"
+
+
+model_kwargs = {"device": "cuda"}
+encode_kwargs = {"normalize_embeddings": True}
+embedding_model = HuggingFaceEmbeddings(
+    model_name=EMBEDDING_MODEL,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs,
+)
+
+
+class ActionWithCromaDB:
+    def __init__(self, embedding, collection_name: str, persist_directory):
+        self.embedding = embedding
+        self.vector_db = Chroma(
+            collection_name=collection_name,
+            embedding_function=self.embedding,
+            persist_directory=persist_directory,
+        )
+        self.collections = {}
+
+    def add_doc(self, documents: list, metadatas: list = None):
+        if metadatas is None:
+            metadatas = [{"date": str(datetime.now())} for _ in documents]
+
+        self.vector_db.add_texts(documents, metadatas=metadatas)
+
+    def query_doc(self, query_text: str, n_results: int = 4):
+        results = self.vector_db.similarity_search(query_text, k=n_results)
+        return results
--- src/utils/pdf_parser.py
+++ src/utils/pdf_parser.py
@@ -4,20 +4,16 @@ from pypdf import PdfReader, PdfWriter
 
 
 def load_documents(path_pdf):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
 
     loader = UnstructuredPDFLoader(path_pdf)
     splitted_data = loader.load_and_split(text_splitter)
     return splitted_data
 
 
-def pdf_clipping(reader, writer, path):
+def pdf_clipping(reader: PdfReader, writer: PdfWriter, path):
     for i in range(69, 103 + 1):
         writer.add_page(reader.pages[i])
 
     with open(path, "wb") as f:
         writer.write(f)
-
-
-res = load_documents(path_pdf="./file.pdf")
-print(res[0])
