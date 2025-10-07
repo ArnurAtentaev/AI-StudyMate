922f9af1a51a35bb7093b9875941d5bde199ee16
Arnur Atentaev <arnurarni@gmail.com>
2025-10-07 23:26:18 +0500

    cleared llm_init.py

--- src/langgraph/nodes.py
+++ src/langgraph/nodes.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 from src.db.common_action import CommonAction
 from src.langgraph.states import GlobalState
+from src.utils.parsers import LineListOutputParser
+from src.langchain.llm_init import initialize_llm
 
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
 
 
 CONFIG = load_dotenv(".env")
@@ -18,10 +21,25 @@ embedding_model = HuggingFaceEmbeddings(
     encode_kwargs=encode_kwargs,
 )
 
+prompt_for_retrieves = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Ты — ИИ-ассистент для поиска информации по документам в базе данных Chroma. "
+            "Сгенерируй 3 варианта исходного вопроса, чтобы улучшить поиск документов. "
+            "Варианты должны быть разными, но сохранять суть основного вопроса.",
+        ),
+        ("human", "Сгенерируй 3 разных переформулировки вопроса: {question}"),
+    ]
+)
+
 
 def retriever_node(state: GlobalState, collection_name):
     db = CommonAction(embedding_model=embedding_model, collection_name=collection_name)
-    db_results = db.query_docs(query_text=state["query"], chain=)
+    llm = initialize_llm(task="text-generation", config="chat")
+    llm_chain = prompt_for_retrieves | llm | LineListOutputParser()
+
+    db_results = db.query_docs(query_text=state["query"], chain=llm_chain)
     state["results"] = db_results
     return state
 
--- src/utils/file_parser.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def pdf_clipping(reader, writer, path: Path):
-    for i in range(69, 103 + 1):
-        writer.add_page(reader.pages[i])
-
-    with open(path, "wb") as f:
-        writer.write(f)
